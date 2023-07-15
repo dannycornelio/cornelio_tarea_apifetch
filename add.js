aa82fc497a10f8403713f67f1f0f1a65d824b0bc
dannycornelio <licdannycornelio@gmail.com>
2023-07-15 12:02:15 -0600

    Obtener los elementos del formulario y del botón

--- add.js
+++ add.js
@@ -1,1 +1,6 @@
-const formulario = document.querySelector('form')
+// Obtener  los elementos del formulario y del botón
+const formulario = document.querySelector('#formulario');
+const botonConsulta = document.querySelector('#botonConsulta');
+
+
+

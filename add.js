ee87a24d10d77250d42ab3518ae61b62b93890c5
dannycornelio <licdannycornelio@gmail.com>
2023-07-15 12:06:03 -0600

    consulta al a api y el even, asi como para optener

--- add.js
+++ add.js
@@ -1,6 +1,20 @@
 // Obtener  los elementos del formulario y del botón
-const formulario = document.querySelector('#formulario');
-const botonConsulta = document.querySelector('#botonConsulta');
+const formulario = document.querySelector('#formulario')
+const botonConsulta = document.querySelector('#botonConsulta')
+
+ // Función para realizar la consulta a la API
+ const consultarApi = async (e) => {
+
+// Evitar que se envíe el formulario por defecto
+    e.preventDefault()
+
+// Obtener la URL y el método del formulario
+    const url = formulario.getAttribute('action')
+    const config = {
+        method: formulario.getAttribute('method')
+    }
+
+}
 
 
 

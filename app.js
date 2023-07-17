922349d866878d50ef7ecc020e5b40b795332393
dannycornelio <licdannycornelio@gmail.com>
2023-07-16 20:03:28 -0600

    MODIFICACION EN EL CODIGO

--- app.js
+++ app.js
@@ -1,48 +1,42 @@
-const boton = document.getElementById('btnGen');
-const texto = document.getElementById('texto');
+const boton = document.getElementById("btnGen");
+const texto = document.getElementById("texto");
 
-
-async function getData(mensaje){
-
+async function getData(mensaje) {
   const respuesta = await fetch(`phpConsulta.php?texto=${mensaje}`);
-    const config = {
-  // aqui se establece el metodo que utilizaremos para traer los datos
-      method: 'GET' 
-        };
-  let data = await respuesta.json();
-//aqui validamos si el metodo utilizado es diferente de GET
-//de ser GET mostrara la Data
-  if (config.method.toUpperCase() === 'GET') {
-    if (data.resultado === true) {
-      mostrarData(data);
-//si la Data esta vacia enviara esta alerta
-    } else {
-      alert("Error en la consulta, intente nuevamente.");
-    }
-// si el metodo no es GET enviara esta alerta    
-  } else {
+  const config = {
+    // aquí se establece el método que utilizaremos para traer los datos
+    method: "POST",
+  };
+
+  // Validación del método
+  if (config.method.toUpperCase() !== "GET") {
     alert("El método de búsqueda no es GET.");
+    return;
+  }
+
+  let data = await respuesta.json();
+
+  // Validación de la data
+  if (data.resultado === true) {
+    mostrarData(data);
+  } else {
+    alert("Error en la consulta, intente nuevamente.");
   }
 }
 
-//aqui almacenamos las variables 
-function mostrarData(data){
+//aqui almacenamos las variables
+function mostrarData(data) {
+  const base64 = document.getElementById("base64");
+  const md5 = document.getElementById("md5");
+  const sha256 = document.getElementById("sha256");
 
-  const base64 = document.getElementById('base64');
-  const md5 = document.getElementById('md5');
-  const sha256 = document.getElementById('sha256');
-  
   //aqui llevalos la data a las variables
   base64.innerHTML = data.base64;
   md5.innerHTML = data.md5;
-  sha256.innerHTML = data.sha256
-
+  sha256.innerHTML = data.sha256;
 }
 
-//cuando se orpima el boton se cargara el valor del mensaje 
-boton.addEventListener('click', function() {
-
+//cuando se orpima el boton se cargara el valor del mensaje
+boton.addEventListener("click", function () {
   getData(texto.value);
-
 });
-
--- phpConsulta.php
+++ phpConsulta.php
@@ -1,40 +1,31 @@
 <?php
 
 try {
-
   $metodo = $_SERVER['REQUEST_METHOD'];
-
   $respuesta = array();
-//si el metodo es identico a GET
+
   if ($metodo === 'GET') {
+    if (isset($_GET['texto']) && $_GET['texto'] != '') {
+      $texto = $_GET['texto'];
 
-//si el texto existe y si es diferente de vacio
-      if(isset($_GET['texto']) && $_GET['texto'] != ''){
-//la variable texto almacenara lo que se llame por el metodo get
-        $texto = $_GET['texto'];
+      $respuesta['resultado'] = true;
+      $respuesta['base64'] = base64_encode($texto);
+      $respuesta['md5'] = md5($texto);
+      $respuesta['sha256'] = hash('sha256', $texto);
 
-//si la repuesta es verdadera cada campo de la tabla convertira los datos al codigo que se le indica
-        $respuesta['resultado'] = true;
-        $respuesta['base64'] = base64_encode($texto);
-        $respuesta['md5'] = md5($texto);
-        $respuesta['sha256'] = hash('sha256',$texto);
-
-        $res = json_encode($respuesta);
-
-        print_r($res);
-
-      }else{
-        //se envia si no existe la variable texto o el valor es vacio
-        $respuesta['resultado'] = false;
-        print_r(json_encode($respuesta));
-      }
-
+      $res = json_encode($respuesta);
+      print_r($res);
+    } else {
+      $respuesta['resultado'] = false;
+      print_r(json_encode($respuesta));
+    }
   } else {
-    // Realiza acciones para otros métodos
-    http_response_code(500);
-    die();
+    $respuesta['resultado'] = false;
+    $respuesta['error'] = "No es un método GET";
+    http_response_code(405);
+    print_r(json_encode($respuesta));
   }
-}catch(Exception $e){
-  echo "Error: ".$e->getMessage()."<br>";
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage() . "<br>";
 }
 ?>
